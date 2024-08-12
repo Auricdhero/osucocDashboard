3580576e9a78aae8f6786a5c61a19871cc0d9a1f
aurictorkornoo <44117263+Auricdhero@users.noreply.github.com>
2024-08-12 20:06:20 +0000

    Update sermon.js

--- src/api/sermon/controllers/sermon.js
+++ src/api/sermon/controllers/sermon.js
@@ -6,15 +6,18 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
+// @ts-ignore
 module.exports = createCoreController('api::sermon.sermon', ({ strapi }) => ({
   async findOne(ctx) {
     const { slug } = ctx.params;
     const entity = await strapi.db.query('api::sermon.sermon').findOne({
-      where: { slug }
+      where: { slug },
+      populate:["Picture", "Audio", "Slides"]
     });
 
     // @ts-ignore
     const sanitizedEntity = await this.sanitizeOutput(entity);
     return this.transformResponse(sanitizedEntity);
-  }
-}));
+  } 
+}
+));
